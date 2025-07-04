15212f6c08c56cfd55a145f049966179cf39ae73
Rahul-9299 <bamrahul92@gmail.com>
2025-07-04 22:19:19 +0545

    Update l1.py

--- l1.py
+++ l1.py
@@ -1,3 +1,4 @@
+#program to remove all duplicates from a list and print the unique elements.
 marks=[1,23,3,2,3,32,5,54,5,4,5,6,]
 unique_marks = list(set(marks))
 print("unique marks:", unique_marks)
