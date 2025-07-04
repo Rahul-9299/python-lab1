c3bdfb7312deee76f9c8a8d55e5656c6348d3cee
Rahul-9299 <bamrahul92@gmail.com>
2025-07-04 22:18:36 +0545

    Update l2.py

--- l2.py
+++ l2.py
@@ -1,3 +1,4 @@
+# program to display the maximum and minimum numbers from the tuple.
 marks=(1,23,4,5,5,6,6,7,8,8,6,5,78,5,54,3,4)
 print("maximum marks:",max(marks))
 print("minimum marks:",min(marks))
